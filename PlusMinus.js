9c09c3d7bc3ee1c3f1bae9b1a43073f06da2553b
김세린 <68180798+SRin23@users.noreply.github.com>
2021-06-21 13:42:24 +0000

    6/21 commit

---
+++ PlusMinus.js
@@ -0,0 +1,12 @@
+//음양구하기
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i = 0; i<signs.length; i++){
+        if(signs[i]===true){
+            answer += absolutes[i];
+        }else{
+            answer -= absolutes[i];
+        }
+    }
+    return answer;
+}
