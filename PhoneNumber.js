b89bc8db364dac7ebba73a1ddaca9ce163ff8f01
김세린 <68180798+SRin23@users.noreply.github.com>
2021-07-26 09:35:38 +0000

    핸드폰 번호 가리기

--- PhoneNumber.js
+++ PhoneNumber.js
@@ -1,1 +1,9 @@
 //핸드폰 번호 가리기
+function solution(phone_number) {
+    var answer = '';
+    for(let i = 0; i<(phone_number.length)-4; i++){
+        answer+='*'
+    }
+    answer += phone_number.slice(phone_number.length-4, phone_number.length)
+    return answer;
+}
